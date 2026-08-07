feb8dc1a44788c7e4de3fb6296a1e6da1a1a1388
Alan Fentanes López <alanfentanes@gmail.com>
2026-08-07 17:00:41 -0600

    Use production Vercel domain for site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://web-1-e4ik9zz3c-alancius-projects.vercel.app',
+  site: 'https://web-1-alancius-projects.vercel.app',
   output: 'static',
   server: {
     host: '0.0.0.0'
