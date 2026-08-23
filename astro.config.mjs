4e27c7e1eb48b72d7484f2a2efe842942dd47ceb
Alan Fentanes López <alanfentanes@gmail.com>
2026-08-22 23:13:18 -0600

    Disable Astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,9 @@ export default defineConfig({
     defaultStrategy: 'viewport'
   },
   integrations: [sitemap()],
+  devToolbar: {
+    enabled: false
+  },
   vite: {
     plugins: [tailwindcss()]
   }
